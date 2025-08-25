04773ba142ccaba7f6b34baf19b0f29f56d5bc41
oscar-suek <153554082+oscar-suek@users.noreply.github.com>
2025-08-25 10:05:32 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const items = ['book','file','fan']
 const nums = [1,2,3,4,5,6,7,8,9,10]
 const evenNums = []
 nb5 = document.querySelector('#nb5')
+bb1 = document.querySelector('#bb1')
+bb2 = document.querySelector('#bb2')
+newB = document.querySelector('#new-b')
+lastB = document.querySelector('#last-b')
 
 for(let i=0; i<6; i++){
     let sw = ''
@@ -17,6 +21,70 @@ for(let i=0; i<7; i++){
         <div class="ballss"></div>
     `
     nb5.innerHTML += qw
+}
+
+for(let i=0; i<1; i++){
+    let rw = ''
+    rw = `
+        <div class="balls1"></div>
+    `
+    bb1.innerHTML += rw
+}
+
+for(let i=0; i<12; i++){
+    let jw = ''
+    jw = `
+        <div class="ballss2"></div>
+    `
+    bb1.innerHTML += jw
+}
+
+for(let i=0; i<5; i++){
+    let rq = ''
+    rq = `
+        <div class="nballs"></div>
+    `
+    bb2.innerHTML += rq
+}
+
+for(let i=0; i<8; i++){
+    let jq = ''
+    jq = `
+        <div class="nballss"></div>
+    `
+    bb2.innerHTML += jq
+}
+
+for(let i=0; i<11; i++){
+    let rd = ''
+    rd = `
+        <div class="new_ball1"></div>
+    `
+    newB.innerHTML += rd
+}
+
+for(let i=0; i<2; i++){
+    let jd = ''
+    jd = `
+        <div class="new_ball2"></div>
+    `
+    newB.innerHTML += jd
+}
+
+for(let i=0; i<6; i++){
+    let zd = ''
+    zd = `
+        <div class="last_ball1"></div>
+    `
+    lastB.innerHTML += zd
+}
+
+for(let i=0; i<7; i++){
+    let ld = ''
+    ld = `
+        <div class="last_ball2"></div>
+    `
+    lastB.innerHTML += ld
 }
 
 
